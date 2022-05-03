21dc08c5cbc4195c1cebba243f2ed83952a2005d
JulesBernal <102987740+JulesBernal@users.noreply.github.com>
2022-05-02 21:37:59 -0400

    final fixes

--- script.js
+++ script.js
@@ -57,46 +57,52 @@ class PersonCl{
     fetchJob(){
         console.log(`${this.name} is a ${this.job}.`);
     }
-    
-    
 }
 class ProgrammerCl extends PersonCl {
     constructor(name,job,age,languages){
         super(name,job,age);
-        this.languages=languages || [];
+       this.languages=languages || [];
         this.busy=true;
     }
     completeTask(){
-        if(this.completeTask.busy){
+        if(this.busy){
             this.busy=false;
             console.log(`${this.name} is no longer busy, please assign a new task!`);
         }else
-        console.log(`${this.name} is not busy.`)
+        console.log(`${this.name} is busy, please try again later.`)
     }
     offerNewTask(){
-        if(this.offerNewTask.busy){
+        if(this.busy){
             console.log(`${this.name} is not busy, would love more responsibility.`);
         }else 
         console.log(`${this.name} is too busy to accept new assignments.`)
     }
-
     learnLanguage(language){
         this.languages.push(language);
         console.log(`${this.name} has learned a new language, ${language}!`);
     }
     listLanguage(){
-        console.log(`${this.name} knows ${this.languages.toString()}`);
+        console.log(`${this.name} knows ${this.languages.toString()}.`);
     }
 }
-const aza = new ProgrammerCl ("Aza","Front End Developer",24);
-
-console.log(`
+const aza = new ProgrammerCl ("Aza","Front End Developer",24,);
+const luce = new ProgrammerCl ("Luce","Server Manager",25);
 
 
 //////Very Hard Section//////`)
 aza.exercise();
 aza.fetchJob();
+aza.completeTask();
+aza.offerNewTask();
 aza.learnLanguage("java");
-
 aza.learnLanguage("c++");
 aza.listLanguage();
+
+console.log(``);
+luce.exercise();
+luce.fetchJob();
+luce.completeTask();
+luce.offerNewTask();
+luce.learnLanguage("python");
+luce.learnLanguage("ruby");
+luce.listLanguage();
